813b4a979d74dcf2aa3bfb9cfe78b4676b607404
Steven Ly <1stevenly92@gmail.com>
2020-04-01 15:55:00 -0700

    added basic file and able to console.log user input

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    var vowels =["a","e", "i", "o", "u", "y" ]
+
+    $("#form").submit(function(event){
+        event.preventDefault();
+        var input = ($("#input").val());
+        var splitInput = input.split("");
+
+
+        console.log(splitInput);
+
+    
+        
+
+
+    })
+    
+
+
+
+
+
+
+
+
+
+})
